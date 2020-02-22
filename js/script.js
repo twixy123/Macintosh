ef0a5aedf0c0521fb5177c470723b514fed1a609
Twixy <luckytwixy321@gmail.com>
2020-02-22 11:02:52 +0500

    start project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let video = document.querySelector('.video-content__video');
+
+video.addEventListener('click', function () {
+    console.log(video);
+    
+    video.setAttribute('controls', '');
+    video.setAttribute('autoplay', '');
+});
+
+
